1c59ecd2e10781a9b1861a10530582f7cd775ddc
Chamdid123 <muhammadchamdid@gmail.com>
2026-07-23 08:02:43 +0300

    margin

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,13 @@
-
-getMembers();
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent = document.lastModified;
+
+const navbutton = document.querySelector('#cham-btn');
+const navlinks = document.querySelector('#cham-bar');
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
